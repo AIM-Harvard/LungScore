507fd5acc950deb8484b85bc9b764196ddbb29c7
ahmed98adly <ahmedadly67@hotmail.com>
2024-04-30 05:51:22 -0400

    utils

--- lungage/utils/risk_groups.py
+++ lungage/utils/risk_groups.py
@@ -3,38 +3,38 @@
 """
 import numpy as np
 
-def define_riskgroups_tuneset(tune_logits):
+def define_riskgroups_tuneset(tune_preds):
     """
     define 5 risk groups based on tuning set
     Args:
-        tune_logits (numpy_array): numpy array with tuning set predictions logits (values from 0 to 1) representing lung age
+        tune_preds (numpy_array): numpy array with tuning set predictions (values from 0 to 1) representing lung age
     Returns:
         print statments with 5 risk groups thresholds
     """
-    print("Very Low risk group <= ", np.quantile(tune_logits, 0.25)) 
-    print("Low risk group <= ", np.quantile(tune_logits, 0.50))
-    print("Intermediate risk group <= ", np.quantile(tune_logits, 0.75))
-    print("High risk group <= ", np.quantile(tune_logits, 0.95))
-    print("Very High risk group <= ", np.quantile(tune_logits, 1)) 
+    print("Very Low risk group <= ", np.quantile(tune_preds, 0.25)) 
+    print("Low risk group <= ", np.quantile(tune_preds, 0.50))
+    print("Intermediate risk group <= ", np.quantile(tune_preds, 0.75))
+    print("High risk group <= ", np.quantile(tune_preds, 0.95))
+    print("Very High risk group <= ", np.quantile(tune_preds, 1)) 
 
     return None
 
 
-def predict_riskgroup(logit):
+def predict_riskgroup(ai_lung_age_score):
     """
     predict risk group based on lung health thresholds from tuning set based on 5 risk groups
     Args:
-        logit (int): (values from 0 to 1) representing lung age
+        ai_lung_age_score (int): (values from 0 to 1) representing lung age
     Returns:
         risk group category
     """
-    if logit <= 0.3239:     
+    if ai_lung_age_score <= 0.3239:     
         risk_group_category = "Group1: Very low risk"
-    elif (logit > 0.3239) and (logit <= 0.4345):   
+    elif (ai_lung_age_score > 0.3239) and (ai_lung_age_score <= 0.4345):   
         risk_group_category = "Group2: Low risk"
-    elif (logit > 0.4345) and (logit <= 0.5478):   
+    elif (ai_lung_age_score > 0.4345) and (ai_lung_age_score <= 0.5478):   
         risk_group_category = "Group3: Intermediate risk" 
-    elif (logit > 0.5478) and (logit <= 0.7123):  
+    elif (ai_lung_age_score > 0.5478) and (ai_lung_age_score <= 0.7123):  
         risk_group_category = "Group4: High risk"
     else:         
         risk_group_category = "Group5: Very High risk"
