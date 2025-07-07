2062cd1fd33c3189d049f00f4ad2973297ec39d4
ahmed98adly <ahmedadly67@hotmail.com>
2025-07-07 11:12:09 -0400

    update stats_analysis

--- stats_analysis/KM.R
+++ stats_analysis/KM.R
@@ -1,75 +1,220 @@
 """
-  Deep-learning biomarker for Lung Health stats_analysis - Plotting KM in NLST
+Deep-learning biomarker for Lung Health stats_analysis - KM analysis
+
+# The code and data of this repository are intended to promote transparent and reproducible research
+# of the paper -- Deep Learning-based Lung Health Quantification on Computed Tomography in Adults
+
+# THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
-.libPaths()
-# Get current library paths
-current_lib_paths <- .libPaths()
-# Add custom directory to library paths
-new_lib_paths <- c("/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs", current_lib_paths)
-# Set the new library paths
-.libPaths(new_lib_paths)
 
 
+"""
+Example usage at the end of this file
+"""
 
-library("Matrix", lib.loc="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs")
-library("survival", lib.loc="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs")
-library(ggplot2)
+
+# load libraries
+library(survival)
 library(lubridate) 
-library(tidyr)
+library(ggplot2)
 library(ggsurvfit)
 library(gtsummary)
-library("survminer", lib.loc="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs")
-library(plotly)
 
-# install.packages("unigd", lib="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs", dependencies=TRUE)
+############################
+# KM Plot function
+#############################
+plot_KM_unzoomed_fivesplit <- function(df, time_col, event_col, group_col, title, xlab, ylab, pdf_file, cohort_name, xlim, ylim = c(0, 1), YEAR = 12) {
+  
+  # Prepare the data frame for plotting
+  df_for_plot <- df
+  df_for_plot$surv_obj <- with(df, Surv(time = df[[time_col]], event = df[[event_col]]))
+  df_for_plot$group <- df[[group_col]]
+  print(df_for_plot$group)
+
+  # Fit survival curves
+  fit_surv <- survfit(surv_obj ~ group, data = df_for_plot)
+  
+  
+  ### SURVIVAL AT specific YEAR USING KAPLAN MEIER ESTIMATOR
+  surv_prob <- summary(fit_surv, times = YEAR)$surv
+  # extract mortality probabilities at 5 years
+  mortality_prob <- 1 - surv_prob
+  
+  
+  
+  # Calculate the total number of included patients
+  total_patients <- nrow(df)
+  
+  # Cox Proportional Hazards Model to get the HR and 95% CI
+  cox_model <- coxph(surv_obj ~ group, data = df_for_plot)
+  print(summary(cox_model))
+  hr <- exp(coef(cox_model))
+  ci <- exp(confint(cox_model))
+
+  
+  pval <- summary(cox_model)$coefficients[,5]
+  
+  # Reciprocal of the Hazard Ratios and Confidence Intervals
+  hr_group1 <- hr[1]
+  hr_group2 <- hr[2]
+  hr_group3 <- hr[3]
+  hr_group4 <- hr[4]
+ 
+  
+  # for 4 risk groups
+  ci_group1 <- c(ci[1], ci[4]) 
+  ci_group2 <- c(ci[2], ci[5])
+  ci_group3 <- c(ci[3], ci[6])
+  ci_group4 <- c(ci[4], ci[7])
+  
+  # Formatting p-values
+  p_text_group1 <- ifelse(pval[1] < 0.001, "<0.001", sprintf("=%.3f", pval[1]))
+  p_text_group2 <- ifelse(pval[2] < 0.001, "<0.001", sprintf("=%.3f", pval[2]))
+  p_text_group3 <- ifelse(pval[3] < 0.001, "<0.001", sprintf("=%.3f", pval[3]))
+  p_text_group4 <- ifelse(pval[4] < 0.001, "<0.001", sprintf("=%.3f", pval[4]))
+  
+  
+  # Creating summary text REVERSED
+  hr_text_group1 <- sprintf("%.2f (%.2f-%.2f)", hr_group1, ci_group1[1], ci_group1[2])
+  hr_text_group2 <- sprintf("%.2f (%.2f-%.2f)", hr_group2, ci_group2[1], ci_group2[2])
+  hr_text_group3 <- sprintf("%.2f (%.2f-%.2f)", hr_group3, ci_group3[1], ci_group3[2])
+  hr_text_group4 <- sprintf("%.2f (%.2f-%.2f)", hr_group4, ci_group4[1], ci_group4[2])
+  
+  
+  # Generate the plot
+  g <- plot_survival_unzoomed(time_col, event_col, fit_surv, df_for_plot, cohort_name, hr_text_group1, hr_text_group2, hr_text_group3, hr_text_group4, total_patients, title, xlab, ylab, group_col, xlim, ylim, surv_prob_1, surv_prob_2, surv_prob_3, surv_prob_4, surv_prob_5, mortality_prob_1, mortality_prob_2, mortality_prob_3, mortality_prob_4, mortality_prob_5, YEAR)
+  
+  # Save plot as PDF without blank page
+  pdf(pdf_file, width = 6, height = 5, onefile = TRUE)
+  print(g) 
+  dev.off()
+  
+  # Perform log-rank test using the original group column
+  surv_diff <- survdiff(surv_obj ~ group, data = df_for_plot)
+  print(surv_diff)
+}
+
+plot_survival_unzoomed <- function(time_col, event_col, fit, data, cohort_name, hr_text_group1, hr_text_group2, hr_text_group3, hr_text_group4, total_patients, title, xlab, ylab, group_col, xlim, ylim, surv_prob_1, surv_prob_2, surv_prob_3, surv_prob_4, surv_prob_5, mortality_prob_1, mortality_prob_2, mortality_prob_3, mortality_prob_4, mortality_prob_5, YEAR) {
+  
+  # calculate degree of freedom
+  # KM are always unadjusted here
+  # change function (NLST or FHS), data, time, and event accordingly
+  p_value_catdf_reduced <- cal_4df_pvalue_unadj(
+    data = data,  # Your dataset
+    time_col = time_col,  # Time-to-event column
+    event_col = event_col,  # Event column
+    group_col = "lunghealth_cat"  # Grouping variable
+  )
+  
+  p_value_catdf_reduced <- ifelse(p_value_catdf_reduced < 0.001, "< 0.001", sprintf("= %.3f", p_value_catdf_reduced))
+  p_value_catdf_reduced <- paste("p ", p_value_catdf_reduced)
+  
+  
+  g <- ggsurvplot(
+    fit,
+    data = data,
+    risk.table = TRUE,
+    #risk.events = TRUE,
+    risk.table.title = "At risk",
+    risk.table.height = 0.2,
+    fontsize = 3.5,
+    conf.int = FALSE,
+    legend = c(0.9, 0.2),
+    legend.title = "Lung Health",
+    
+    legend.labs = c("very high", "high", "moderate", "low", "very low"),
+    palette = c("#1b7837","#5aae61", "#a6dba0", "#c2a5cf", "#762a83"),
+    
+    
+    title = paste(title, cohort_name),
+    xlab = xlab,
+    ylab = ylab,
+    tables.theme = theme_cleantable() + theme(plot.title = element_text(size = 12)),
+    censor = FALSE,
+    xlim = xlim,
+    ylim = c(0, 1)
+  )
+  
+  g$plot <- g$plot + 
+    scale_x_continuous(expand = c(0,0)) +
+    scale_y_continuous(expand = c(0,0), labels = scales::percent_format(scale = 100)) +
+    # change ylim
+    scale_y_continuous(limits = ylim, expand = c(0,0), labels = scales::percent_format(scale = 100)) +
+    theme(legend.position = "none") +
+    
+    # very high
+    annotate("text", x = 0.1, y = (0.25 * (1-ylim[[1]]) + ylim[[1]]), label = paste("high:"), hjust = "left", size = 3.5) + 
+    annotate("text", x = 2, y = (0.25 * (1-ylim[[1]]) + ylim[[1]]), label = paste("Ref"), hjust = "left", size = 3.5)
+
+    # high group
+    annotate("text", x = 0.1, y = (0.2  * (1-ylim[[1]]) + ylim[[1]]), label = paste("high:"), hjust = "left", size = 3.5) +
+    annotate("text", x = 2, y = (0.2  * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group1, hjust = "left", size = 3.5) +
+    
+    # moderate group
+    annotate("text", x = 0.1, y = (0.15  * (1-ylim[[1]]) + ylim[[1]]), label = paste("moderate:"), hjust = "left", size = 3.5) +
+    annotate("text", x = 2, y = (0.15 * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group2, hjust = "left", size = 3.5) +
+    
+    # low group
+    annotate("text", x = 0.1, y = (0.1  * (1-ylim[[1]]) + ylim[[1]]), label = paste("low:"), hjust = "left", size = 3.5) +
+    annotate("text", x = 2, y = (0.1  * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group3, hjust = "left", size = 3.5) +
+    
+    # very low group
+    annotate("text", x = 0.1, y = (0.05  * (1-ylim[[1]]) + ylim[[1]]), label = paste("very low:"), hjust = "left", size = 3.5) +
+    annotate("text", x = 2, y = (0.05 * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group4, hjust = "left", size = 3.5) +
+    
+    # 4 df p-value
+    annotate("text", x = 5.2, y = (0.20 * (1-ylim[[1]]) + ylim[[1]]), label = p_value_catdf_reduced, hjust = "left", size = 3.5) +
+    
+    
+    # no of patients and HZ 95 ci 
+    annotate("text", x = 0.1, y = (0.35 * (1-ylim[[1]]) + ylim[[1]]), label = paste("n=", total_patients), hjust = "left", size = 3.5) + 
+    annotate("text", x = 2, y = (0.3  * (1-ylim[[1]]) + ylim[[1]]), label = paste("HR (95% CI)"), hjust = "left", size = 3.5) +
+    
+  
+  return(g)
+}
 
 
-###########
-# library(httpgd)
-# install.packages("Matrix", repos = "http://R-Forge.R-project.org", lib="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs", dependencies=TRUE)
-# install.packages("survminer", lib="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs", dependencies=TRUE)
-# library("survminer", lib.loc="/home/ahmed/R/x86_64-pc-linux-gnu-library/4.1/languageserver/libs")
-#################
+# Define a function to calculate 4 degree of freedom (4df) p-values in FHS or NLST unadjusted
+cal_4df_pvalue_unadj <- function(data, time_col, event_col, group_col) {
+  
+  # Fit the full Cox model including the group variable (no adjustment)
+  formula <- as.formula(paste("Surv(", time_col, ",", event_col, ") ~", group_col))
+  COX_model_full <- coxph(formula, data = data)
+  
+  # Fit the reduced model excluding the group variable (intercept-only model)
+  formula_reduced <- as.formula(paste("Surv(", time_col, ",", event_col, ") ~ 1"))
+  COX_model_reduced <- coxph(formula_reduced, data = data)
+  
+  # Perform a likelihood ratio test between the full and reduced models
+  LRT <- anova(COX_model_full, COX_model_reduced, test = "LRT")
+  
+  # Extract the 4df p-value
+  p_value <- LRT[2, 4]  
+  
+  return(p_value)
+}
 
 
-# load data
-data <- read.csv('/mnt/data6/DeepPY/src_main/stats_analysis/smokeAI_NLST_test.csv') 
+# Example usage for oevrall survival in NLST
+# read data
+df_NLST <- read.csv('/mnt/data/NLST_test.csv')
 
-# AI lung health 5 risk groups
-data$DeepPY <- factor(data$Deep_py)
-data$DeepPY
-
-# # Get overall survival
-data$death[which(data$death>1)] = 0
-
-# Get cancer death
-data$ci[which(data$ci!='1')] = 0
-data$ci[which(data$ci=='1')] = 1
-data$ci = as.numeric(data$ci)
-
-
-# KM OS AI lung health
-
-fit <- survfit(Surv(fup, death) ~ DeepPY, data = data)
-my_colors <- c("skyblue3", "blue3", "red1", "tomato", "darkred")
-
-res <- ggsurvplot(fit, data = data, risk.table = TRUE,  conf.int = FALSE, censor = FALSE,
-                  ylim = c(0.4, 1), xlim = c(0, 5110), xscale = "d_y", break.x.by = 365.3,
-                  legend.labs = c("Very Low", "Low", "Moderate", "High", "Very High"), 
-                  legend.title = "n=2581\n\nHazard ratio\nVery Low: Reference\nLow: 2.27 (95% CI, 0.93-1.26), P=0.072\nModerate: 2.27 (95% CI, 0.93-1.26), P=0.072\nHigh: 2.27 (95% CI, 0.93-1.26), P=0.072\nVery High: 2.27 (95% CI, 0.93-1.26), P=0.072",
-                  legend = c(0.22, 0.21),
-                  tables.theme = theme_cleantable(),
-                  risk.table.height = 0.25, xlab = 'Time (Years)', ylab = 'ACM Free Survival',
-                  palette = my_colors) 
-
-# Modify legend and plot aesthetics
-res$plot <- res$plot + theme(legend.key.width = unit(0, "mm"),
-                             legend.key.height = unit(0, "mm"),
-                             legend.text = element_blank())
-
-
-print(res)
-
-# jpeg(filename="/mnt/data6/DeepPY/src_main/km.jpeg", width=600, height=400)
-# print(res)
-# dev.off()
+# OS NLST - Figure 2b
+plot_KM_unzoomed_fivesplit(
+  df = df_NLST, 
+  time_col = "OSYEARS_12y", 
+  event_col = "DEATH_12y", 
+  group_col = "lunghealth_cat",  # 5 risk groups 
+  title = "Overall Survival in", 
+  xlab = "Years since enrollment", 
+  ylab = "Percentage Surviving", 
+  pdf_file = "mnt/outcome_analysis/Figures_LH/OS_KM_NLST.pdf", 
+  cohort_name = "NLST",
+  xlim = c(0, 12), 
+  ylim = c(0.4,1)
+)
---
+++ stats_analysis/KM_incidence.R
@@ -0,0 +1,236 @@
+"""
+Deep-learning biomarker for Lung Health stats_analysis - KM incidence analysis
+
+# The code and data of this repository are intended to promote transparent and reproducible research
+# of the paper -- Deep Learning-based Lung Health Quantification on Computed Tomography in Adults
+
+# THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+
+"""
+Example usage at the end of this file
+"""
+
+
+# load libraries
+library(survival)
+library(lubridate) 
+library(ggplot2)
+library(ggsurvfit)
+library(gtsummary)
+
+############################
+# KM Incidence Plot function
+#############################
+plot_KM_unzoomed_fivesplit_incidence <- function(df, time_col, event_col, group_col, title, xlab, ylab, pdf_file, cohort_name, xlim, ylim = c(0, 1), offset_annotation = 0.1, offset_annotation_distance = 5, YEAR = 12) {
+
+# Prepare the data frame for plotting
+df_for_plot <- df
+df_for_plot$surv_obj <- with(df, Surv(time = df[[time_col]], event = df[[event_col]]))
+df_for_plot$group <- df[[group_col]]
+
+# Fit survival curves
+#fit_surv <- survfit(surv_obj ~ group_rev, data = df_for_plot)
+fit_surv <- survfit(surv_obj ~ group, data = df_for_plot)
+
+
+### SURVIVAL AT specific YEAR USING KAPLAN MEIER ESTIMATOR
+surv_prob <- summary(fit_surv, times = YEAR)$surv
+# extract mortality probabilities at 5 years
+mortality_prob <- 1 - surv_prob
+
+# Calculate the total number of included patients
+total_patients <- nrow(df)
+
+# Cox Proportional Hazards Model to get the HR and 95% CI
+cox_model <- coxph(surv_obj ~ group, data = df_for_plot)
+print(summary(cox_model))
+hr <- exp(coef(cox_model))
+ci <- exp(confint(cox_model))
+print(ci)
+pval <- summary(cox_model)$coefficients[,5]
+
+# Reciprocal of the Hazard Ratios and Confidence Intervals
+hr_group1 <- hr[1]
+hr_group2 <- hr[2]
+hr_group3 <- hr[3]
+hr_group4 <- hr[4]
+
+
+# for 4 risk groups
+ci_group1 <- c(ci[1], ci[5]) 
+ci_group2 <- c(ci[2], ci[6])
+ci_group3 <- c(ci[3], ci[7])
+ci_group4 <- c(ci[4], ci[8])
+
+
+# Formatting p-values
+p_text_group1 <- ifelse(pval[1] < 0.001, "<0.001", sprintf("=%.3f", pval[1]))
+p_text_group2 <- ifelse(pval[2] < 0.001, "<0.001", sprintf("=%.3f", pval[2]))
+p_text_group3 <- ifelse(pval[3] < 0.001, "<0.001", sprintf("=%.3f", pval[3]))
+p_text_group4 <- ifelse(pval[1] < 0.001, "<0.001", sprintf("=%.3f", pval[4]))
+
+
+# Creating summary text REVERSED
+hr_text_group1 <- sprintf("%.2f (%.2f-%.2f)", hr_group1, ci_group1[1], ci_group1[2])
+hr_text_group2 <- sprintf("%.2f (%.2f-%.2f)", hr_group2, ci_group2[1], ci_group2[2])
+hr_text_group3 <- sprintf("%.2f (%.2f-%.2f)", hr_group3, ci_group3[1], ci_group3[2])
+hr_text_group4 <- sprintf("%.2f (%.2f-%.2f)", hr_group4, ci_group4[1], ci_group4[2])
+
+
+# Generate the plot
+g <- plot_survival_unzoomed_incidence(time_col, event_col, fit_surv, df_for_plot, cohort_name, hr_text_group1, hr_text_group2, hr_text_group3, hr_text_group4, total_patients, title, xlab, ylab, group_col, xlim, ylim, offset_annotation, offset_annotation_distance, surv_prob_1, surv_prob_2, surv_prob_3, surv_prob_4, surv_prob_5, mortality_prob_1, mortality_prob_2, mortality_prob_3, mortality_prob_4, mortality_prob_5, YEAR)
+
+# Save plot as PDF without blank page
+pdf(pdf_file, width = 6, height = 5, onefile = TRUE)
+print(g) 
+dev.off()
+
+# Perform log-rank test using the original group column
+surv_diff <- survdiff(surv_obj ~ group, data = df_for_plot)
+print(surv_diff)
+}
+
+plot_survival_unzoomed_incidence <- function(time_col, event_col, fit, data, cohort_name, hr_text_group1, hr_text_group2, hr_text_group3, hr_text_group4, total_patients, title, xlab, ylab, group_col, xlim, ylim, offset_annotation, offset_annotation_distance, surv_prob_1, surv_prob_2, surv_prob_3, surv_prob_4, surv_prob_5, mortality_prob_1, mortality_prob_2, mortality_prob_3, mortality_prob_4, mortality_prob_5, YEAR) {
+
+# calculate degree of freedom
+# KM are always unadjusted here
+p_value_catdf_reduced <- cal_4df_pvalue_unadj(
+  data = data,  # Your dataset
+  time_col = time_col,  # Time-to-event column
+  event_col = event_col,  # Event column
+  group_col = "lunghealth_cat"  # Grouping variable
+)
+
+p_value_catdf_reduced <- ifelse(p_value_catdf_reduced < 0.001, "< 0.001", sprintf("= %.3f", p_value_catdf_reduced))
+p_value_catdf_reduced <- paste("p ", p_value_catdf_reduced)
+
+
+
+# Transforming survival probability into cumulative incidence
+fit_transformed <- fit
+fit_transformed$surv <- 1 - fit_transformed$surv
+
+g <- ggsurvplot(
+  fit_transformed,
+  data = data,
+  risk.table = TRUE,
+  #risk.events = TRUE,
+  risk.table.title = "At risk",
+  risk.table.height = 0.2,
+  fontsize = 3.5,
+  conf.int = FALSE,
+  legend = c(0.9, 0.2),
+  legend.title = "Lung Health",
+  
+  legend.labs = c("very high", "high", "moderate", "low", "very low"),
+  palette = c("#1b7837", "#5aae61", "#a6dba0", "#c2a5cf","#762a83"),
+  
+  
+  title = paste(title, cohort_name),
+  xlab = xlab,
+  ylab = ylab,
+  tables.theme = theme_cleantable() + theme(plot.title = element_text(size = 12)),
+  censor = FALSE,
+  xlim = xlim,
+  ylim = c(0, 1)
+)
+
+g$plot <- g$plot + 
+
+  scale_x_continuous(expand = c(0,0)) +
+  scale_y_continuous(expand = c(0,0), labels = scales::percent_format(scale = 100)) +
+  # change ylim
+  scale_y_continuous(limits = ylim, expand = c(0,0), labels = scales::percent_format(scale = 100)) +
+  theme(legend.position = "none") +
+  
+  # very high
+  annotate("text", x = 0.1, y = (offset_annotation + 0.025 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = paste("very high:"), hjust = "left", size = 3.5) + 
+  annotate("text", x = 2, y = (offset_annotation + 0.025 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = paste("Ref"), hjust = "left", size = 3.5)
+
+  # high group
+  annotate("text", x = 0.1, y = (offset_annotation + 0.02 /offset_annotation_distance  * (1-ylim[[1]]) + ylim[[1]]),  label = paste("high:"), hjust = "left", size = 3.5) +
+  annotate("text", x = 2, y = (offset_annotation + 0.02 /offset_annotation_distance  * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group1, hjust = "left", size = 3.5) +
+  
+  # moderate group
+  annotate("text", x = 0.1, y = (offset_annotation + 0.015 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = paste("moderate:"), hjust = "left", size = 3.5) +
+  annotate("text", x = 2, y = (offset_annotation + 0.015 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group2, hjust = "left", size = 3.5) +
+  
+  # low group
+  annotate("text", x = 0.1, y = (offset_annotation + 0.01  /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = paste("low:"), hjust = "left", size = 3.5) +
+  annotate("text", x = 2, y = (offset_annotation + 0.01  /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group3, hjust = "left", size = 3.5) +
+  
+  # very low group
+  annotate("text", x = 0.1, y = (offset_annotation + 0.005 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = paste("very low:"), hjust = "left", size = 3.5) +
+  annotate("text", x = 2, y =  (offset_annotation + 0.005 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = hr_text_group4, hjust = "left", size = 3.5) +
+  
+  # 4 df p-value
+  annotate("text", x = 5.2, y = (offset_annotation + 0.005 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = p_value_catdf_reduced, hjust = "left", size = 3.5) +
+  
+  
+  # no of patients and HZ 95 ci 
+  annotate("text", x = 0.1, y = (offset_annotation + 0.035 /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]), label = paste("n=", total_patients), hjust = "left", size = 3.5) + 
+  annotate("text", x = 2, y = (offset_annotation + 0.03  /offset_annotation_distance * (1-ylim[[1]]) + ylim[[1]]),label = paste("HR (95% CI)"), hjust = "left", size = 3.5) +
+  
+
+    
+return(g)
+}
+
+
+# Define a function to calculate 4 degree of freedom (4df) p-values in FHS or NLST unadjusted
+cal_4df_pvalue_unadj <- function(data, time_col, event_col, group_col) {
+  
+  # Fit the full Cox model including the group variable (no adjustment)
+  formula <- as.formula(paste("Surv(", time_col, ",", event_col, ") ~", group_col))
+  COX_model_full <- coxph(formula, data = data)
+  
+  # Fit the reduced model excluding the group variable (intercept-only model)
+  formula_reduced <- as.formula(paste("Surv(", time_col, ",", event_col, ") ~ 1"))
+  COX_model_reduced <- coxph(formula_reduced, data = data)
+  
+  # Perform a likelihood ratio test between the full and reduced models
+  LRT <- anova(COX_model_full, COX_model_reduced, test = "LRT")
+  
+  # Extract the 4df p-value
+  p_value <- LRT[2, 4]  
+  
+  return(p_value)
+}
+
+
+###########################################################
+
+# Example usage for lung cancer death in NLST
+# read data
+df_NLST <- read.csv('/mnt/data/NLST_test.csv')
+
+# Lung cancer death NLST - Figure 3b
+plot_KM_unzoomed_fivesplit_incidence(
+df = df_NLST, 
+time_col = "ICD_OSYEARS_12y",   
+event_col = "LUNGCANCER_DEATH_12y", 
+group_col = "lunghealth_cat", # 5 risk groups
+title = "Lung cancer death in", 
+xlab = "Years since randomization", 
+ylab = "Cumulative lung cancer mortality", 
+pdf_file = "mnt/outcome_analysis/Figures_LH/LC_DEATH_KM_NLST.pdf", 
+cohort_name = "NLST",
+xlim = c(0, 12), 
+ylim = c(0, 0.15),
+offset_annotation = 0.1,
+offset_annotation_distance = 0.8,
+YEAR = 12
+)
+
+
+
+
+
+
+
